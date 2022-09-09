4cba14242681987d56f22e0f37c18cdd2f4a4901
Rafael Andrei Schmitz <rafaelandreischmitz@gmail.com>
2022-09-09 11:12:33 -0300

    [ INIT ]

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="./public/css/style.css">
+        <link href="http://fonts.cdnfonts.com/css/montserrat" rel="stylesheet">
+        <script src="./public/js/jquery.min.js"></script>
+        <script src="./public/js/main.js"></script>
+    </head>
+    <body>
+        <!-- <div class="lds-ellipsis"><div><span></span><span></span><span></span><span></span></div></div> -->
+
+        <main>
+            <h1>Quem é o mais <b>lindo</b> do casal?</h1>
+            
+            <div class="buttons">
+                <button id="rafa"><span>RAFA</span></button>
+                <button id="lu"><span>LU</span></button>
+            </div>
+        </main>
+
+        <div class="notify"></div>
+    </body>
+</html>
---
+++ public/js/main.js
@@ -0,0 +1,54 @@
+$(function() {
+    setTimeout(function(){
+        $('.lds-ellipsis div').addClass('toTop');
+    
+        setTimeout(function(){
+            $('.lds-ellipsis').addClass('toBottom');
+        }, 450);
+    
+        setTimeout(function(){
+            $('.lds-ellipsis').remove();
+        }, 1500);
+    }, 1500);
+
+    var winWidth = window.innerWidth - 100;
+    var winHeight = window.innerHeight - 100;
+
+    $('button#lu').on('mouseover', function(){
+        $('button#lu').addClass('absolute');
+
+        randomTop = getRandomNumber($('body').outerHeight(), winHeight);
+        randomLeft = getRandomNumber(0, winWidth);
+        
+        $('button#lu').attr('style', 'top: '+randomTop+'px; left: '+randomLeft+'px; transform: none');
+    });
+    
+    function getRandomNumber(min, max) {
+        return Math.random() * (max - min) + min;
+    }
+
+    $('button#rafa span').on('click', function(){
+        handleNotify("Acertou!");
+    });
+
+    $('button#lu span').on('click', function(){
+        handleNotify("Como tu conseguiu clicar???");
+    });
+
+    function handleNotify(message){
+        $('.notify').text(message);
+        $('.notify').addClass('show');
+
+        setTimeout(function(){
+            $('.notify').addClass('popup');
+        }, 1);
+
+        setTimeout(function(){
+            $('.notify').removeClass('popup');
+        }, 1000);
+    
+        setTimeout(function(){
+            $('.notify').removeClass('show');
+        }, 1300);
+    }
+});
